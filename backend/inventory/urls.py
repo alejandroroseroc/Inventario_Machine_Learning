b4271115c64ff2e73263ec5788e0b09bf0f36e05
nelson.chaves@camousucc.edu.co <nelson.chaves@campusucc.edu.co>
2025-10-31 11:42:21 -0500

    Agregando lotes al proyecto

--- backend/inventory/urls.py
+++ backend/inventory/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import KPIView
 from .views_products import ProductoListCreateView, RecalcularProductosView
+from .views_lotes import LoteListCreateView
 
 urlpatterns = [
     path("panel/kpis", KPIView.as_view(), name="panel-kpis"),
     path("inventory/productos", ProductoListCreateView.as_view(), name="productos"),
     path("inventory/productos/recalcular", RecalcularProductosView.as_view(), name="productos-recalcular"),
+    path("inventory/lotes", LoteListCreateView.as_view(), name="lotes-list-create"),
 ]

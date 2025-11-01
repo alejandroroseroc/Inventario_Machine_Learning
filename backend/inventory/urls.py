4f6583e02b4a3dd27101deb7270e1df7eae9d065
nelson.chaves@camousucc.edu.co <nelson.chaves@campusucc.edu.co>
2025-10-31 22:35:50 -0500

    Agregando movimientos al repositorio

--- backend/inventory/urls.py
+++ backend/inventory/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from .views import KPIView
 from .views_products import ProductoListCreateView, RecalcularProductosView
 from .views_lotes import LoteListCreateView
+from .views_movimientos import MovimientoListCreateView
 
 urlpatterns = [
     path("panel/kpis", KPIView.as_view(), name="panel-kpis"),
     path("inventory/productos", ProductoListCreateView.as_view(), name="productos"),
     path("inventory/productos/recalcular", RecalcularProductosView.as_view(), name="productos-recalcular"),
     path("inventory/lotes", LoteListCreateView.as_view(), name="lotes-list-create"),
+    path("inventory/movimientos", MovimientoListCreateView.as_view(), name="movimientos-list-create"),
 ]
